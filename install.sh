b80e150dab541a14f493238d7f49a1a39a2d7f9f
Jesse Donat <j.donat@capstonedigital.com>
2013-07-12 17:14:44 -0500

    better headings and composer.phar added

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-COLOR='\033[01;31m'	# bold red
+GREEN='\033[01;32m'	#  purpLle
+PURPL='\033[01;35m'	#  purpLle
+RED='\033[01;31m'	#  red
 RESET='\033[00;00m'	# normal white
 
 OMZ=$HOME/.oh-my-zsh
@@ -9,7 +11,35 @@ DOTPATH=`dirname $0`
 
 if [ -d "$OMZ" ]; then
 
-	echo "oh-my-zsh found!";
+	printf "===$PURPL Installing/Replacing Config Files     $RESET===\n"
+
+	cp $DOTPATH/zsh/.zshrc $HOME
+	cp $DOTPATH/.aliases $HOME
+	cp $DOTPATH/.tmux.conf $HOME
+
+	printf "===$GREEN                  Done                 $RESET===\n\n"
+
+
+	# ---
+
+
+	printf "===$PURPL Creating ~/bin and ~/Scripts          $RESET===\n"
+
+	if [ ! -d "$HOME/bin" ]; then
+		mkdir $HOME/bin
+	fi
+
+	if [ ! -d "$HOME/Scripts" ]; then
+		mkdir $HOME/Scripts
+	fi
+
+	printf "===$GREEN                  Done                 $RESET===\n\n"
+
+
+	# ------
+
+
+	printf "===$PURPL Installing ZSH Theme                  $RESET===\n"
 
 	if [ ! -d "$OMZ/custom" ]; then
 		echo "mkdir custom"
@@ -23,49 +53,67 @@ if [ -d "$OMZ" ]; then
 
 	cp $DOTPATH/zsh/jdonat.zsh-theme $OMZ/custom/themes
 
+	printf "===$GREEN                  Done                 $RESET===\n\n"
+
+
+	# ------
+
+
+	printf "===$PURPL Installing ZSH Syntax Highlighting    $RESET===\n"
+
 	if [ ! -d "$OMZ/custom/plugins" ]; then
 		echo "mkdir custom/plugins"
 		mkdir $OMZ/custom/plugins
 	fi
 
-	printf "\nInstalling ZSH Syntax Highlighting...\n\n"
 	rm -rf $OMZ/custom/plugins/zsh-syntax-highlighting
 	git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $OMZ/custom/plugins/zsh-syntax-highlighting
 
-	cp $DOTPATH/zsh/.zshrc $HOME
-	cp $DOTPATH/.aliases $HOME
-	cp $DOTPATH/.tmux.conf $HOME
+	printf "===$GREEN                  Done                 $RESET===\n\n"
 
 
-	if [ ! -d "$HOME/bin" ]; then
-		mkdir $HOME/bin
-	fi
+	# ------
 
-	if [ ! -d "$HOME/Scripts" ]; then
-		mkdir $HOME/Scripts
-	fi
 
 	RUBYPATH=`which ruby`
 	if [ -x "$RUBYPATH" ]; then
-		
-		printf "\nInstalling Open Directory Scanner...\n\n"
+
+		printf "===$PURPL Installing Open Directory Scanner     $RESET===\n"
+
 		rm -rf $HOME/Scripts/opendir
 		git clone git://gist.github.com/3285130.git $HOME/Scripts/opendir
+
+		printf "===$GREEN                  Done                 $RESET===\n\n"
 		
 	else
-		printf "$COLOR - Ruby is not installed $RESET\n"
+		printf "$RED - Ruby is not installed $RESET\n"
 	fi
+
+	# ---
 
 	PHPPATH=`which php`
 	if [ -x "$PHPPATH" ]; then
 		
+		printf "===$PURPL Installing misc php scripts           $RESET===\n"
+
 		cp $DOTPATH/Scripts/argsr $HOME/Scripts
+
+		printf "===$GREEN                  Done                 $RESET===\n\n"
+
+
+		# -----
+
+		printf "===$PURPL Installing composer                   $RESET===\n"
+
+		curl -sS https://getcomposer.org/installer | php -- --install-dir=$HOME/bin
+
+		printf "===$GREEN                  Done                 $RESET===\n\n"
 		
 	else
-		printf "$COLOR - PHP is not installed $RESET\n"
+		printf "$RED - PHP is not installed $RESET\n"
 	fi
 
 
 else
-	printf "$COLOR - oh-my-zsh is not installed $RESET\n"
+	printf "$RED - oh-my-zsh is not installed $RESET\n"
 fi
