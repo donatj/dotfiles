8084c9b51b001dc14dde72ee38aae2a512502ce4
Jesse Donat <donatj@gmail.com>
2014-11-06 15:53:57 -0600

    formatting

--- join2insert.php
+++ join2insert.php
@@ -3,30 +3,30 @@
 
 $string = $argv[1];
 $string = strtolower($string);
-$string = str_replace(array("\r", "\n"), ' ', $string);
+$string = str_replace(array( "\r", "\n" ), ' ', $string);
 
 $onparts = explode(" on ", $string, 2);
-$j = explode(" and ", $onparts[1]);
+$j       = explode(" and ", $onparts[1]);
 
-$ins = array();
+$ins  = array();
 $outs = array();
-foreach($j as $k) {
+foreach( $j as $k ) {
 	// print_r($k);
 	$equalparts = explode("=", $k, 2);
 	// print_r($equalparts);
-	$ins[] = trim($equalparts[0]);
+	$ins[]  = trim($equalparts[0]);
 	$outs[] = trim($equalparts[1]);
 }
 
-$ins_tbl_parts = explode(".", $ins[0], 2);
-$table1 = $ins_tbl_parts[0];
-
+$ins_tbl_parts  = explode(".", $ins[0], 2);
 $outs_tbl_parts = explode(".", $outs[0], 2);
+
+$table1 = $ins_tbl_parts[0];
 $table2 = $outs_tbl_parts[0];
 
 $in  = implode(", \n\t", $ins);
 $out = implode(", \n\t", $outs);
 
-echo "INSERT INTO `{$table1}` \n(\n\t{$in}\n) \n\n(SELECT\n\t{$out}\nFROM `{$table2}`)"; 
+echo "INSERT INTO `{$table1}` \n(\n\t{$in}\n) \n\n(SELECT\n\t{$out}\nFROM `{$table2}`)";
 
 echo PHP_EOL;
