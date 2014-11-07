7e5602e0ec0b05895641be943ffda09aee7544c0
Jesse Donat <donatj@gmail.com>
2014-11-07 11:32:28 -0600

    Better preliminary support for multitable selects

--- join2insert.php
+++ join2insert.php
@@ -10,17 +10,21 @@ while( $line = fgets(STDIN) ) {
 $string = strtolower($string);
 $string = str_replace(array( "\r", "\n" ), ' ', $string);
 
-$onparts = explode(" on ", $string, 2);
-$j       = explode(" and ", $onparts[1]);
+$joinparts = preg_split('/\s(inner |outer |left | right)?join\s.*?\son\s/', $string);
 
 $ins  = array();
 $outs = array();
-foreach( $j as $k ) {
-	// print_r($k);
-	$equalparts = explode("=", $k, 2);
-	// print_r($equalparts);
-	$ins[]  = trim($equalparts[0]);
-	$outs[] = trim($equalparts[1]);
+array_shift($joinparts);
+foreach( $joinparts as $joinpart ) {
+	$j = explode(" and ", $joinpart);
+
+	foreach( $j as $k ) {
+		// print_r($k);
+		$equalparts = explode("=", $k, 2);
+		// print_r($equalparts);
+		$ins[]  = trim($equalparts[0]);
+		$outs[] = trim($equalparts[1]);
+	}
 }
 
 $ins_tbl_parts  = explode(".", $ins[0], 2);
@@ -35,3 +39,9 @@ $out = implode(", \n\t", $outs);
 echo "INSERT INTO `{$table1}` \n(\n\t{$in}\n) \n\n(SELECT\n\t{$out}\nFROM `{$table2}`)";
 
 echo PHP_EOL;
+
+function see( $data ) {
+	echo "\n\n---V---\n";
+	var_export($data);
+	echo "\n---X---\n\n";
+}
