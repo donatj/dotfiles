24f3b7b165570e8dba4b1ae6c2cad8f56d2f7f29
Jesse Donat <jdonat@capstonedigital.com>
2015-02-12 11:35:00 -0600

    Adds git aliases

--- install.sh
+++ install.sh
@@ -41,6 +41,18 @@ wget --no-check-certificate http://install.ohmyz.sh -O - | sh
 
 if [ -d "$OMZ" ]; then
 
+	printf "===$PURPL Configuring Git                       $RESET===\n"
+
+	git config --global --replace-all alias.dp "diff --patience"
+	git config --global --replace-all alias.cp "cherry-pick"
+	git config --global --replace-all alias.cb "checkout -b"
+
+	printf "===$GREEN                  Done                 $RESET===\n\n"
+
+
+	# ---
+
+
 	printf "===$PURPL Installing/Replacing Config Files     $RESET===\n"
 
 	_mv_file_rm_sym $HOME/.zshrc
