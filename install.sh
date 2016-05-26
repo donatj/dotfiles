1f076f0f5aaa631f826a4f9abf92d03683aacdda
Jesse Donat <donatj@gmail.com>
2016-05-26 14:49:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ if [ -d "$OMZ" ]; then
 	git config --global --replace-all alias.cp "cherry-pick"
 	git config --global --replace-all alias.cplog 'log --oneline --reverse'
 	git config --global --replace-all alias.cb "checkout -b"
-	git config --global --replace-all alias.exclude '{ echo $1 >> $(git rev-parse --git-dir)/info/exclude }; f'
+	git config --global --replace-all alias.exclude '!f(){ echo $1 >> $(git rev-parse --git-dir)/info/exclude; }; f'
 
 	printf "===$GREEN                  Done                 $RESET===\n\n"
 
