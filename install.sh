7c0389736012839e1e81560bb8d3faa948f71dae
Jesse Donat <donatj@gmail.com>
2016-05-26 15:41:17 -0500

    Moves git setup into it's own file

--- install.sh
+++ install.sh
@@ -48,13 +48,7 @@ if [ -d "$OMZ" ]; then
 
 	printf "===$PURPL Configuring Git                       $RESET===\n"
 
-	git config --global --replace-all core.excludesfile "$HOME/.gitignore_global"
-
-	git config --global --replace-all alias.dp "diff --patience"
-	git config --global --replace-all alias.cp "cherry-pick"
-	git config --global --replace-all alias.cplog 'log --oneline --reverse'
-	git config --global --replace-all alias.cb "checkout -b"
-	git config --global --replace-all alias.exclude '!f(){ echo $1 >> $(git rev-parse --git-dir)/info/exclude; }; f'
+	$DOTPATH/git/setup.sh
 
 	printf "===$GREEN                  Done                 $RESET===\n\n"
 
